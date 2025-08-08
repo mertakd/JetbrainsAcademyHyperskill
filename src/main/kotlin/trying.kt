e1de81810f991cb31f9bac3d8b1ef0142946d5dd
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2025-08-08 13:22:52 +0300

    ForLoopAndRangesQuiz.ipynb

--- src/main/kotlin/trying.kt
+++ src/main/kotlin/trying.kt
@@ -2,88 +2,53 @@ import java.util.Scanner
 
 fun main1() {
 
+    val start = readLine()?.toIntOrNull() ?: return
+    val end = readLine()?.toIntOrNull() ?: return
 
-    val a = readLine()!!.toInt()
-    val b = readLine()!!.toInt()
-    val n = readLine()!!.toInt()
-
-    var count = 0
-
-    for (i in a..b){
-        if (i % n == 0)
-            count++
-    }
-
-    println(count)
-}
-
-
-fun main2() {
-
-    val scanner = Scanner(System.`in`)
-    val a = scanner.nextInt()
-    val b = scanner.nextInt()
-    val n = scanner.nextInt()
-
-    println((a..b).count{it % n == 0})
-}
-
-fun main3() {
-    val a = readln().toInt()
-    val b = readln().toInt()
-    val n = readln().toInt()
-
-    println((a..b).filter { it % n == 0}.size)
-}
-
-
-fun main4() {
-    val a = readLine()!!.toInt()
-    val b = readLine()!!.toInt()
-
-
-    var sum = 0
-
-    for (i in a..b){
-        sum += i
-    }
-
-    println(sum)
-}
-
-
-fun main5() {
-    val a = readln().toInt()
-    val b = readln().toInt()
-
-    var product = 1L
-
-    for (i in a until b){
-        product *= i
-    }
-
-    println(product)
-}
-
-
-fun main6() {
-    for (i in 1..3) {
-        for (j in 1..i) {
-            print(j)
+    for (number in start..end) {
+        when {
+            number % 3 == 0 && number % 5 == 0 -> println("FizzBuzz")
+            number % 3 == 0 -> println("Fizz")
+            number % 5 == 0 -> println("Buzz")
+            else -> println(number)
         }
     }
 }
 
 
-fun main() {
+fun main2() {
 
-    val n = readln().toIntOrNull() ?: return
-    var sum = 0
+    val n = readLine()?.toIntOrNull() ?: return
+    var min = Int.MAX_VALUE
 
-    for (i in 1..n){
-        val number = readln().toIntOrNull() ?: return
-        sum += number
+    for (i in 1..n) {
+        val number = readLine()?.toIntOrNull() ?: return
+        if (number < min) {
+            min = number
+        }
     }
 
-    println(sum)
+    println(min)
+}
+
+
+fun main() {
+    val n = readLine()?.toIntOrNull() ?: return
+    var isSorted = true
+    var previousNumber = Int.MIN_VALUE
+
+    for (i in 1..n) {
+        val number = readLine()?.toIntOrNull() ?: return
+        if (number < previousNumber) {
+            isSorted = false
+            break
+        }
+        previousNumber = number
+    }
+
+    if (isSorted) {
+        println("YES")
+    } else {
+        println("NO")
+    }
 }
