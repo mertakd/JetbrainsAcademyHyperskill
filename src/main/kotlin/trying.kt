434a2fb3617dd32a26bb6cf7784506650b429f3f
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2025-08-15 21:25:11 +0300

    ListTopicAndQuiz.ipynb

--- src/main/kotlin/trying.kt
+++ src/main/kotlin/trying.kt
@@ -1,25 +1,20 @@
+fun solution(products: List<String>, product: String) {
+
+    val positions = mutableListOf<Int>()
+
+    for (i in products.indices) {
+        if (products[i] == product) {
+            positions.add(i)
+        }
+    }
+    println(positions.joinToString(" "))
+}
+
+
 fun main() {
 
-    val southernCross = mutableListOf("Acrux", "Gacrux", "Imai", "Mimosa")
-    val stars = mutableListOf("Ginan", "Mu Crucis")
-    val names = mutableListOf("Jack", "John", "Katie")
-    val food = mutableListOf("Bread", "Cheese", "Meat")
-    val fruits = mutableListOf("Apple", "Banana", "Grape", "Mango")
-
-    southernCross.removeAt(0)
-    southernCross.remove("Mimosa")
-
-    stars.add("New star")
-    stars.add(0, "First star")
-
-    names.clear()
-
-    food.addAll(fruits)
-
-    println(names)                // []
-    println(southernCross)        // [Gacrux, Imai]
-    println(stars)                // [First star, Ginan, Mu Crucis, New star]
-    println(food)                 // [Bread, Cheese, Meat, Apple, Banana, Grape, Mango]
-
-
+    //cevaba yazma
+    val products = listOf("Mustard", "Cheese", "Eggs", "Cola", "Eggs")
+    val productToFind = "Eggs"
+    solution(products, productToFind)
 }
